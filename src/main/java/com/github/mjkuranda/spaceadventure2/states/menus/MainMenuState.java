70dbccc9b7e74bc51c3a9d7b674aaffce810977a
mjkuranda <kuranda.marek@gmail.com>
2023-07-26 18:51:06 +0200

    game version and creator (:

--- src/main/java/com/github/mjkuranda/spaceadventure2/states/menus/MainMenuState.java
+++ src/main/java/com/github/mjkuranda/spaceadventure2/states/menus/MainMenuState.java
@@ -8,6 +8,7 @@ import com.github.mjkuranda.spaceadventure2.states.menus.options.events.EnterSta
 import com.github.mjkuranda.spaceadventure2.states.menus.options.events.ExitGameMenuOptionEvent;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class MainMenuState extends MenuState {
@@ -28,11 +29,31 @@ public class MainMenuState extends MenuState {
 
     @Override
     public void renderPartials(GameContainer container, Graphics g) {
+        g.setFont(GameFont.VCR_OSD_MONO);
+        float fontHeight = GameFont.VCR_OSD_MONO.getHeight("X");
+
         String title = "Space Adventure II";
-        int width = GameFont.VCR_OSD_MONO.getWidth(title);
+        int width = getStringWidth(title, GameFont.VCR_OSD_MONO);
         float textX = (float) container.getWidth() / 2 - (float) width / 2;
 
-        g.setFont(GameFont.VCR_OSD_MONO);
+        String version = "v0.13.0";
+        int versionWidth = getStringWidth(version, GameFont.VCR_OSD_MONO);
+        float versionX = (float) container.getWidth() - versionWidth;
+        float versionY = (float) container.getHeight() - fontHeight - fontHeight;
+
+        String creator = "Created by: Marek Kuranda";
+        int creatorWidth = getStringWidth(creator, GameFont.VCR_OSD_MONO);
+        float creatorX = (float) container.getWidth() - creatorWidth;
+        float creatorY = (float) container.getHeight() - fontHeight;
+
+        // Game texts
         g.drawString(title, textX, 300);
+        g.drawString(version, versionX, versionY);
+        g.drawString(creator, creatorX, creatorY);
+        g.fillRect(container.getWidth() - 24, creatorY, 2, 4);
+    }
+
+    private int getStringWidth(String str, UnicodeFont font) {
+        return font.getWidth(str);
     }
 }
