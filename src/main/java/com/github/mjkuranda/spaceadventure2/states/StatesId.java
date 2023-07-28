cdb5889831d499c362f421e2beec2f36e5422367
mjkuranda <kuranda.marek@gmail.com>
2023-07-28 21:09:04 +0200

    game over

--- src/main/java/com/github/mjkuranda/spaceadventure2/states/StatesId.java
+++ src/main/java/com/github/mjkuranda/spaceadventure2/states/StatesId.java
@@ -3,7 +3,6 @@ package com.github.mjkuranda.spaceadventure2.states;
 public interface StatesId {
     int INTRO = 0;
     int GAME = 1;
-    int GAME_OVER = 2;
 
     int MAIN_MENU = 10;
     int GAME_OVER_MENU = 11;
