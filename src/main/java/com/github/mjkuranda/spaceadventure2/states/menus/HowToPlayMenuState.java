1b575a7c2f683e4d56c0a01a02c4b35d52c82cc8
mjkuranda <kuranda.marek@gmail.com>
2023-08-14 14:34:58 +0200

    How to play done

--- src/main/java/com/github/mjkuranda/spaceadventure2/states/menus/HowToPlayMenuState.java
+++ src/main/java/com/github/mjkuranda/spaceadventure2/states/menus/HowToPlayMenuState.java
@@ -1,10 +1,12 @@
 package com.github.mjkuranda.spaceadventure2.states.menus;
 
+import com.github.mjkuranda.spaceadventure2.resources.GameFont;
 import com.github.mjkuranda.spaceadventure2.states.StatesId;
 import com.github.mjkuranda.spaceadventure2.states.highscore.HighScoreHandler;
 import com.github.mjkuranda.spaceadventure2.states.menus.options.MenuOption;
 import com.github.mjkuranda.spaceadventure2.states.menus.options.SimpleMenuOption;
 import com.github.mjkuranda.spaceadventure2.states.menus.options.events.EnterStateMenuOptionEvent;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -44,6 +46,23 @@ public class HowToPlayMenuState extends MenuState {
 
     @Override
     public void renderPartials(GameContainer container, Graphics g) {
+        renderSection(container, g, 128, "Player navigation", new String[] { "A - go left", "D - go right", "SPACE - shoot" });
+        renderSection(container, g, 256, "Modify GUI", new String[] { "F - toggle fullscreen mode", "H - current FPS count", "M - toggle mesh" });
+        renderSection(container, g, 400, "Rendering", new String[] { "1 - Arcade style rendering", "2 - Retro style rendering" });
+        renderSection(container, g, 512, "Manipulate game", new String[] { "G - spawn a new asteroid" });
+    }
 
+    public void renderSection(GameContainer container, Graphics g, float y, String title, String[] info) {
+        float x = container.getWidth() / 2.0f - 128;
+
+        g.setColor(Color.white);
+        g.setFont(GameFont.VCR_OSD_MONO);
+        g.drawString(title.toUpperCase(), x, y);
+
+        for (int i = 0; i < info.length; i++) {
+            String text = info[i];
+
+            g.drawString(text, x, y + (i+1) * 16);
+        }
     }
 }
