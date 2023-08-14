0b172e0237558b1ab69d1e6ab43ea2ef1eff5afc
mjkuranda <kuranda.marek@gmail.com>
2023-08-14 14:47:10 +0200

    Sign next to input label

--- src/main/java/com/github/mjkuranda/spaceadventure2/states/menus/options/values/StringMenuOptionValue.java
+++ src/main/java/com/github/mjkuranda/spaceadventure2/states/menus/options/values/StringMenuOptionValue.java
@@ -2,6 +2,8 @@ package com.github.mjkuranda.spaceadventure2.states.menus.options.values;
 
 public class StringMenuOptionValue extends MenuOptionValue<String> {
 
+    public static final int MAX_LENGTH = 16;
+
     public StringMenuOptionValue(String value) {
         super(value);
     }
