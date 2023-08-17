94ea6b78cf0d535e21d8c3cfe75e672dc5dd19e6
mjkuranda <kuranda.marek@gmail.com>
2023-08-18 00:02:05 +0200

    Key sounds

--- src/main/java/com/github/mjkuranda/spaceadventure2/resources/GameSound.java
+++ src/main/java/com/github/mjkuranda/spaceadventure2/resources/GameSound.java
@@ -11,6 +11,12 @@ public class GameSound implements GameResource<Sound> {
     public static Sound EXPLOSION_ASTEROID;
     public static Sound EXPLOSION_ASTEROID_2;
     public static Sound GAME_OVER;
+    public static Sound KEY_2;
+    public static Sound KEY_3;
+    public static Sound KEY_4;
+    public static Sound KEY_BACKSPACE;
+    public static Sound KEY_SPACE_BAR;
+    public static Sound KEY_W;
     public static Sound SHOOT;
 
     private Sound sound;
@@ -28,6 +34,12 @@ public class GameSound implements GameResource<Sound> {
         EXPLOSION_ASTEROID = new GameSound("explosion-asteroid.ogg").get();
         EXPLOSION_ASTEROID_2 = new GameSound("explosion-asteroid-2.ogg").get();
         GAME_OVER = new GameSound("game-over.ogg").get();
+        KEY_2 = new GameSound("key2.ogg").get();
+        KEY_3 = new GameSound("key3.ogg").get();
+        KEY_4 = new GameSound("key4.ogg").get();
+        KEY_BACKSPACE = new GameSound("key-backspace.ogg").get();
+        KEY_SPACE_BAR = new GameSound("key-spacebar.ogg").get();
+        KEY_W = new GameSound("keyw.ogg").get();
         SHOOT = new GameSound("shoot.ogg").get();
     }
 
